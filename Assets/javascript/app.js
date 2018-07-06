a68aabc5cbcc9645d8f097722aec62725ddf7af2
Nigel Tufnel <tenorideout@gmail.com>
2018-07-06 14:02:24 -0400

    final commit - all working

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
 
-
-    // I have been unable to get GIF START/STOP to function. I'm sure it's something small but....
-
     // declare global variables. 
 
     var express = ["happiness", "boredom", "glee", "grumpiness"];
@@ -38,20 +35,25 @@ $(document).ready(function () {
         console.log(expressEnt);
         var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + expressEnt;
 
+        // clear columns for new Gif's
+        
         $("#gifLeft").empty();
         $("#gifRight").empty();
+
+        // loop to populate left column of Gif's and give them their class and ID:
 
         for (j = 0; j < 5; j++) {
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function (response) {
-
-                var imageUrl = response.data.image_original_url;
+                console.log(response);
+                var animateUrl = response.data.image_original_url;
+                var stillUrl = response.data.images.original_still.url;
                 var expressionGif = $("<img>");
-                expressionGif.attr("src", imageUrl);
-                expressionGif.attr("data-still", imageUrl);
-                expressionGif.attr("data-animate", imageUrl);
+                expressionGif.attr("src", stillUrl);
+                expressionGif.attr("data-still", stillUrl);
+                expressionGif.attr("data-animate", animateUrl);
                 expressionGif.attr("data-state", "still");
                 expressionGif.attr("class", "gif left");
                 expressionGif.attr("alt", "expression");
@@ -60,46 +62,51 @@ $(document).ready(function () {
 
         }
 
+        // loop to populate right column of Gif's and give them their class and ID:
+
         for (j = 0; j < 5; j++) {
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function (response) {
 
-                var imageUrl = response.data.image_original_url;
+                var animateUrl = response.data.image_original_url;
+                var stillUrl = response.data.images.original_still.url;
                 var expressionGif = $("<img>");
-                expressionGif.attr("src", imageUrl);
-                expressionGif.attr("data-still", imageUrl);
-                expressionGif.attr("data-animate", imageUrl);
+                expressionGif.attr("src", stillUrl);
+                expressionGif.attr("data-still", stillUrl);
+                expressionGif.attr("data-animate", animateUrl);
                 expressionGif.attr("data-state", "still");
                 expressionGif.attr("class", "gif right");
                 expressionGif.attr("alt", "expression");
                 $("#gifRight").prepend(expressionGif);
             })
 
-            // starts and stops GIF's
-
-            $(".gif").on("click", function () {
-
-                var state = $(this).attr("data-state");
-                console.log(state);
+           
 
 
-                if (state === ("still")) {
-                    $(this).attr("src", $(this).attr("data-animate"));
-                    $(this).attr("data-state", "animate");
-                }
-                else if (state === ("animate")) {
-                    $(this).attr("src", $(this).attr("data-still"));
-                    $(this).attr("data-state", "still");
-
-                }
-            })
 
         }
 
     })
 
+     // code that will start and stop GIF's:
 
+    $("body").on("click", ".gif", function () {
+
+        var state = $(this).attr("data-state");
+        console.log(state);
+
+
+        if (state === ("still")) {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }
+        else if (state === ("animate")) {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+
+        }
+    })
 
 })
---
+++ README.md
@@ -0,0 +1,11 @@
+## Rutgers CS Bootcamp Assigment
+
+## GIFTASTIC
+
+# Vale Rideout - 06-2018
+
+* This program will ask user to select and emotion from a column of buttons to pupulate two columns of 5 Gif's each - and then click on them to start and stop their motion. 
+
+* User may enter a new emotion in the blank, and after pressing 'Enter', a new button with that name will be entered into the column of expressions from which to choose their Gifs. 
+
+* By clicking on a GIF, user may toggle their 'animated' state from 'animate' to 'still'.
